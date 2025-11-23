9c91dc47f7a85380ea2a3e6e0613e1b41f5d1b77
HamimBhai742 <omagootruelove147@gmail.com>
2025-11-23 19:56:54 +0600

    Modified

--- back-end/src/app.ts
+++ back-end/src/app.ts
@@ -1,14 +1,13 @@
 import express, { Request, Response } from 'express';
 import 'dotenv/config';
+import { router } from './routes/routes';
 export const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use()
+app.use('/api/v1', router);
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Luxiery Real Estate Server Running.........');
 });
-
-
