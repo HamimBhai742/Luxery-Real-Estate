8888b72abd0502e7439e6c1b508872aaafbcaa2f
HamimBhai742 <omagootruelove147@gmail.com>
2025-12-04 18:15:05 +0600

    update home Page

--- front-end/src/components/FeaturedSection/FeaturedSection.tsx
+++ front-end/src/components/FeaturedSection/FeaturedSection.tsx
@@ -1,7 +1,12 @@
-import React from 'react';
-import PropertiesCard from '../Properties/Card';
+import { Property } from '@/types/property';
+import PropertyCard from '../Properties/PropertyCard';
+import Link from 'next/link';
 
-const FeaturedSection = () => {
+const FeaturedSection = async () => {
+  const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/property`);
+  const data = await res.json();
+  const properties = data.data.properties.slice(0, 3);
+  console.log(properties);
   return (
     <section className='relative py-20 px-4 sm:px-6 lg:px-8'>
       <div className='max-w-7xl mx-auto'>
@@ -14,9 +19,17 @@ const FeaturedSection = () => {
           </p>
         </div>
         <div className='grid grid-cols-1 md:grid-cols-3 gap-8'>
-          {[1, 2, 3].map((item) => (
-            <PropertiesCard key={item} item={item} />
+          {properties.map((property: Property) => (
+            <PropertyCard key={property.id} property={property} />
           ))}
+        </div>
+        <div className='text-center'>
+          <Link
+            href='/properties'
+            className='mt-8 text-lg inline-block dark:bg-amber-600 dark:hover:bg-amber-700 bg-blue-600 hover:bg-blue-700 text-white font-semibold py-3 px-6 rounded'
+          >
+            View All Properties
+          </Link>
         </div>
       </div>
     </section>
